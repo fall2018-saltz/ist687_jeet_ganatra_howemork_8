f3b77c5a113504763a537ad2836be8093c063b82
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-23 13:23:19 -0400

    Saving linearmodel.R

--- linearmodel.R
+++ linearmodel.R
@@ -4,4 +4,6 @@ linmod
 summary(linmod)
 #r-squared = 0.6702
 #adjusted r-squared: = 0.6682
-#based on the p-values found during regression, the hotel state variables, hotel size and gender variables are statistically significant
+
+#based on the p-values found during regression, the hotel state variables, hotel size and 
+#gender variables are statistically significant
