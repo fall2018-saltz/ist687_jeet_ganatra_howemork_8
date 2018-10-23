ef7939564689d4b041fc4129de4ce91a77853542
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-23 13:21:23 -0400

    Saving linearmodel.R

--- linearmodel.R
+++ linearmodel.R
@@ -2,3 +2,5 @@
 linmod  <- lm(overallCustSat~hotelSize+checkInSat+hotelState+hotelClean+hotelFriendly+gender+guestAge+lengthOfStay+whenBookedTrip,data = dataset)
 linmod
 summary(linmod)
+#r-squared = 0.6702
+#adjusted r-squared: = 0.6682
