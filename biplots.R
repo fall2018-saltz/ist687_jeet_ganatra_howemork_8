48cfa24a2fc781ade4ad7eb1403c1faa1af1e6d7
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-23 14:34:28 -0400

    Saving biplots.R

--- biplots.R
+++ biplots.R
@@ -56,3 +56,15 @@ biplot8 <- biplot8 + geom_point()
 biplot9 <- ggplot(dataset,aes(jitter(whenBookedTrip),overallCustSat))
 biplot9 <- biplot9 + geom_point()
 
+# plot1 - most of the observations lie in the 5-10 range but there are a few outliers 
+# plot2 -  most of the observations again lie in the 5-10 range with a few outliers 
+# plot3 - the states on an average have similar average values but California, Pennsylvania and West 
+# Virginia have slightly higher averages
+# plot4 - there are more outliers here in the hotel clean variable and it is more spread out
+# plot5 - the customer satisfaction is seen increasing as the friendliness score increases
+# plot6 - males on an average have a marginally higher satisfaction rating than women
+# plot7 - most guests lie in the 40-60 age group with a few outliers
+# plot8 - the average length of stay is 1-4 nights 
+# plot9 - there is no specific pattern to the data suggesting that it is not an accurate predictor
+
+
