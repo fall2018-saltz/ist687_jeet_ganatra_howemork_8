b394923b75b0b2eee15e3ec636bbed6d2a190f18
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-22 23:40:29 -0400

    Saving biplots.R

--- biplots.R
+++ biplots.R
@@ -1,3 +1,5 @@
 
 library(ggplot2)
-biplot1 <- ggplot(dataset,aes(jitter(hotelSize),overallCustSat)) + geom_point()
+biplot1 <- ggplot(dataset,aes(jitter(hotelSize),overallCustSat))
+biplot1 <- biplot1 + geom_point()
+biplot2 <- ggplot(dataset,aes(jitter(checkInSat),overallCustSat)) + geom_point() 
